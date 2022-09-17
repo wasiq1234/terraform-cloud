3a9e610276a3757c9ca939a0829b8ddfb02330a2
wasiq1234 <107514837+wasiq1234@users.noreply.github.com>
2022-09-17 15:47:40 -0500

    added changes

--- rds/main.tf
+++ rds/main.tf
@@ -69,3 +69,34 @@ resource "aws_db_instance" "default" {
 	db_subnet_group_name = aws_db_subnet_group.db.name
 	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported 
 }
+
+
+
+
+
+
+
+
+
+
+resource "aws_rds_cluster" "example" {
+  cluster_identifier = "example"
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "13.6"
+  database_name      = "test"
+  master_username    = "test"
+  master_password    = "must_be_eight_characters"
+
+  serverlessv2_scaling_configuration {
+    max_capacity = 1.0
+    min_capacity = 0.5
+  }
+}
+
+resource "aws_rds_cluster_instance" "example" {
+  cluster_identifier = aws_rds_cluster.example.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.example.engine
+  engine_version     = aws_rds_cluster.example.engine_version
+}
